86cd427d30187835023b0335d5d5cc03c8a7bc9a
ZVengin <wenjiezhong717@gamil.com>
2025-05-15 17:59:11 +0900

    rename the bash file and delete the run_singularity.sh file

--- run.sh
+++ run.sh
@@ -12,14 +12,12 @@ function split_dataset() {
 }
 
 function create_dataset() {
-    window_size=9
     mkdir -p ${code_dir}/dataset/proc_data_wz${window_size}
     run_creation="python extract2.py \
     --window-size ${window_size} \
     --split-info ${data_dir}/tourism_conversation/split_info.json \
     --annotation-dir ${data_dir}/tourism_conversation/annotations \
-    --output-dir ${data_dir}/proc_data_wz${window_size} \
-    --balance-data"
+    --output-dir ${data_dir}/proc_data_wz${window_size}_wo_balance"
     singularity exec --nv --home ${code_dir}/container/kelvin --workdir ${code_dir}/container --bind ${code_dir}:/tmp/code ${code_dir}/tourism_project.sif bash -c "cd /tmp/code/dataset && ${run_creation}"
 }
 
@@ -34,9 +32,9 @@ function train_seq2seq3() {
     run_train="torchrun \
     --standalone \
     --nproc_per_node=${nproc_per_node} \
-    train.py \
-    --train_file ${data_dir}/proc_data_wz${window_size}/train.json \
-    --dev_file ${data_dir}/proc_data_wz${window_size}/dev.json \
+    train_baseline.py \
+    --train_file ${data_dir}/proc_data_wz${window_size}_wo_balance/train.json \
+    --dev_file ${data_dir}/proc_data_wz${window_size}_wo_balance/dev.json \
     --model_name ${model_name} \
     --batch_size ${batch_size} \
     --accumulated_size ${accumulated_size} \
@@ -44,8 +42,8 @@ function train_seq2seq3() {
     --epochs ${epochs} \
     --optimize_direction min \
     --init_eval_score 100000 \
-    --log_dir ${code_dir}/exps/train-${model_type}-${window_size}-bz$((batch_size * accumulated_size * nproc_per_node))-lr${lr}-ep${epochs} \
-    --project_name TravelDialogue \
+    --log_dir ${code_dir}/baseline_exps/train-${model_type}-${window_size}-bz$((batch_size * accumulated_size * nproc_per_node))-lr${lr}-ep${epochs} \
+    --project_name TravelDialogueBaseline \
     --run_name train-${model_type}-${window_size}-bz$((batch_size * accumulated_size * nproc_per_node))-lr${lr}-ep${epochs}"
     singularity exec --nv --home ${code_dir}/container/kelvin --workdir ${code_dir}/container --bind ${code_dir}:/tmp/code ${code_dir}/tourism_project.sif bash -c "cd /tmp/code && ${run_train}"
 
@@ -56,16 +54,16 @@ function test_seq2seq3() {
     run_train="torchrun \
     --standalone \
     --nproc_per_node=${nproc_per_node} \
-    test.py \
-    --test_file ${data_dir}/proc_data_wz${window_size}/test.json \
+    test_baseline.py \
+    --test_file ${data_dir}/proc_data_wz${window_size}_wo_balance/test.json \
     --key_val_file ${code_dir}/value_set2.json \
     --model_name ${model_name} \
     --batch_size 1 \
     --only_load_model \
     ${constraint_decode} \
     --eval_out ${eval_out} \
-    --log_dir ${code_dir}/exps/train-${model_type}-${window_size}-bz$((batch_size * accumulated_size * nproc_per_node))-lr${lr}-ep${epochs} \
-    --project_name TravelDialogue \
+    --log_dir ${code_dir}/baseline_exps/train-${model_type}-${window_size}-bz$((batch_size * accumulated_size * nproc_per_node))-lr${lr}-ep${epochs} \
+    --project_name TravelDialogueBaseline \
     --run_name test-${model_type}-${eval_out}-wz${window_size}-bz$((batch_size * accumulated_size * nproc_per_node))-lr${lr}-ep${epochs}"
     singularity exec --nv --home ${code_dir}/container/kelvin --workdir ${code_dir}/container --bind ${code_dir}:/tmp/code ${code_dir}/tourism_project.sif bash -c "cd /tmp/code && ${run_train}"
 
@@ -73,22 +71,39 @@ function test_seq2seq3() {
 #split_dataset
 #create_dataset
 #train
-#model_type=mt5-base
-#model_name='google/mt5-base'
+model_type=mt5-base
+model_name='google/mt5-base'
 #model_type=bart-base
-#model_name=facebook/bart-base
-model_type=mt0-base
-model_name=bigscience/mt0-base
+#model_name=ku-nlp/bart-base-japanese
+#model_type=mt0-base
+#model_name=bigscience/mt0-base
 #window_size=1
 batch_size=64
-lr=3e-5
+lr=9e-5
 epochs=20
 accumulated_size=1
 nproc_per_node=1
-#constraint_decode="--constraint_decode"
-#eval_out=eval_constraint_decode
-eval_out=eval
+constraint_decode="--constraint_decode"
+eval_out=eval_constraint_decode
+#eval_out=eval
 
-test_seq2seq3
+if [ $1 == 'train' ];
+then
+  train_seq2seq3
+elif [ $1 == 'test' ];
+then
+  test_seq2seq3
+elif [ $1 == 'split_dataset' ];
+then
+  split_dataset
+elif [ $1 == 'create_dataset' ];
+then
+  create_dataset
+else
+  echo "$1 is invalid mode!"
 #train_seq2seq3
+#test_seq2seq3
+
+
+
 
--- run_singularity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#module load singularity
-
-code_dir=/home/u00483/repos/TourismDialogue
-
-#singularity shell --nv --home ${code_dir}/container/kelvin --workdir ${code_dir}/container --bind ${code_dir}:/tmp/code ${code_dir}/tourism_project.sif
-
-singularity exec --nv --home ${code_dir}/container/kelvin --workdir ${code_dir}/container --bind ${code_dir}:/tmp/code ${code_dir}/tourism_project.sif bash -c "cd browser && export FLASK_APP=app && export PATH=${PATH}:~/.local/bin && flask run --port 9300"
